e789e026dffc1578410760119b1b347770c1a200
issakuss <issakuss@gmail.com>
2021-01-15 11:40:47 +0900

    fix: Fix NeoVim install and URL

--- neovim/setup.sh
+++ neovim/setup.sh
@@ -1,7 +1,7 @@
-brew install neovim
+brew install --HEAD neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-svn export https://github.com/issakuss/setup/branches/master/vim/dotfiles
+svn export https://github.com/issakuss/setup/branches/master/neovim/dotfiles
 mkdir -p ~/dotfiles
 cp -r dotfiles/ ~/dotfiles/
 rm -rf dotfiles
