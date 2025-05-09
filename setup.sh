63b73914e3a2a08058291b3c331cc7dcb2d4f2a9
issakuss <issakuss@gmail.com>
2025-05-09 11:52:33 +0900

    🔬 refactor: Unify files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo -n "Setup file version is 1.2.2.0\n"
+echo -n "Setup file version is 2.0.0.0\n"
+
+echo -n "Connect to ATR network? [Y/n]: "
+read ATRNET
 
 echo -n "Password to unzip: "
 read PASS
@@ -8,54 +11,61 @@ read PASS
 echo -n "Install VPN? [Y/n]: "
 read VPN
 
-# Install installers
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/issakuss/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
-cd ~/Desktop
-
-brew install git
-brew install svn
-git config --global user.name issakuss
-git config --global user.email issakuss@gmail.com
-
-# SSH setting
-ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/ ~/icloud
-cp -rf ~/icloud/ssh/ ~/.ssh/
-chmod 600 ~/.ssh/id_rsa
-
-# dotfiles
-cd ~/
-git clone git@github.com:issakuss/dotfiles.git 
-zsh dotfiles/setup.sh
-cd ~/Desktop
-
-# Get files
-svn export https://github.com/issakuss/setup/branches/main/macos/attaches ~/Desktop/attaches
-cp ~/icloud/private-setup.zip private-setup.zip
-unzip -P $PASS private-setup.zip
-rm -f private-setup.zip
-
-# VPN
 case $VPN in
   "" | [Yy]* )
-    zsh -c "$(curl -fsSL https://raw.githubusercontent.com/issakuss/setup/master/vpn/setup.sh)"
+    echo -n "Password for VPN: "
+    read VPNPASS
     ;;
   * )
     ;;
 esac
 
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/issakuss/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+cd ~/Desktop
+
+# Install Git
+brew install git
+git config --global user.name issakuss
+git config --global user.email issakuss@gmail.com
+
+# Alias SSH
+ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/ ~/icloud
+cp -rf ~/icloud/ssh/ ~/.ssh/
+chmod 600 ~/.ssh/id_rsa
+
+# Clone dotfiles
+cd ~/
+git clone git@github.com:issakuss/dotfiles.git 
+zsh dotfiles/setup.sh
+
+# Get files
+cd ~/Desktop
+svn export https://github.com/issakuss/setup/branches/main/attaches ~/Desktop/attaches
+cp ~/icloud/private-setup.zip private-setup.zip
+unzip -P $PASS private-setup.zip
+rm -f private-setup.zip
+
+# VPN
+
 # Install applications
 zsh -c "$(curl -fsSL https://raw.githubusercontent.com/issakuss/setup/master/adobe/setup.sh)"
-zsh -c "$(curl -fsSL https://raw.githubusercontent.com/issakuss/setup/master/vscode/setup.sh)"
-brew install --cask notion
+brew install openconnect
 brew install issakuss/papnt/papnt
+brew install google-japanese-ime
+brew install docker
+brew install docker-compose
+brew install --cask visual-studio-code
+brew install --cask notion
+brew install --cask notion-mail
 brew install --cask slack
 brew install --cask google-chrome
 brew install --cask bettertouchtool
 brew install --cask karabiner-elements
-brew install docker
-brew install docker-compose
+brew install --cask adobe-creative-cloud
+brew install --cask adobe-creative-cloud-cleaner-tool
 brew install --cask microsoft-word
 brew install --cask microsoft-excel
 brew install --cask microsoft-powerpoint
@@ -72,7 +82,32 @@ brew install --cask font-ricty-diminished
 cp -r private/font/ ~/Library/Fonts/
 rm -rf private/font/
 
-# Application settings
+# Docker settings
+case $ATRNET in
+  "" | [Yy]* )
+    docker context create ncd10 --docker 'host=ssh://issakuss@ncd10'
+    docker context use ncd10
+    ;;
+  * )
+    ;;
+esac
+
+# Adobe settings
+ln -s cp ~/icloud/picture/myswatch.ai /Applications/Adobe\ Illustrator\ 2023/Presets.localized/ja_JP/スウォッチ
+
+# VPN settings
+case $VPN in
+  "" | [Yy]* )
+    chmod 777 ~/Desktop/connect_to_atr.command
+    mv ~/Desktop/connect_to_atr.command /Applications/
+    sed -i '' -e "s/ENTERPASSWORDHERE/${VPNPASS}/g" connect_to_atr.command
+    mv connect_to_atr.command /Applications/
+    ;;
+  * )
+    ;;
+esac
+
+# Quicklook settings
 ## https://github.com/sindresorhus/quick-look-plugins
 ## https://github.com/whomwah/qlstephen
 xattr -d -r com.apple.quarantine ~/Library/QuickLook
@@ -97,6 +132,8 @@ defaults write com.apple.dock showAppExposeGestureEnabled -bool true
 defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 defaults write com.apple.mail minSizeKB 3000
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
 
 # cleanup
 cd ~/Desktop
